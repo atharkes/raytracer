78c7818297de7aec67c04fdb113fb4c184b42f65
Theo <taharkes@gmail.com>
2021-09-22 13:00:22 +0200

    Moved: RecursiveDistancePDF

--- PathTracer/Pathtracing/PDFs/DistancePDFs/IDistancePDF.cs
+++ PathTracer/Pathtracing/PDFs/DistancePDFs/IDistancePDF.cs
@@ -14,4 +14,34 @@
             return new ProductDistancePDF(left, right);
         }
     }
+
+    public interface IRecursiveDistancePDF<T> : IPDF<T> {
+        IPDF<T> Left { get; }
+        IPDF<T> Right { get; }
+
+        new double Probability(T sample) {
+            if (!Contains(sample)) {
+                return 0;
+            } else {
+                double pLeft = Left.Probability(sample);
+                double pRight = Right.Probability(sample);
+                // If there would be an early out for multiplication with 0 (without having to evaluate the recursive calls), the final else statement would be suffice
+                if (pLeft <= 0 && pRight <= 0) {
+                    return 0;
+                } else if (pLeft <= 0) {
+                    return (1 - Left.CumulativeDistribution(sample)) * pRight;
+                } else if (pRight <= 0) {
+                    return (1 - Right.CumulativeDistribution(sample)) * pLeft;
+                } else {
+                    return (1 - Left.CumulativeDistribution(sample)) * pRight + (1 - Right.CumulativeDistribution(sample)) * pLeft;
+                }
+            }
+        }
+
+        new double CumulativeDistribution(T sample) {
+            double l = Left.CumulativeDistribution(sample);
+            double r = Right.CumulativeDistribution(sample);
+            return l + r - l * r;
+        }
+    }
 }
--- PathTracer/Pathtracing/PDFs/IPDF.cs
+++ PathTracer/Pathtracing/PDFs/IPDF.cs
@@ -1,5 +1,4 @@
-﻿using PathTracer.Pathtracing.PDFs.DistancePDFs;
-using System;
+﻿using System;
 
 namespace PathTracer.Pathtracing.PDFs {
     public interface IPDF {
@@ -22,35 +21,5 @@ namespace PathTracer.Pathtracing.PDFs {
         (T1, T2) SampleDouble(Random random);
 
         IPDF<T2> ExtractPDF(T1 sample);
-    }
-
-    public interface IRecursivePDF<T> : IPDF<T> {
-        IPDF<T> Left { get; }
-        IPDF<T> Right { get; }
-
-        new double Probability(T sample) {
-            if (!Contains(sample)) {
-                return 0;
-            } else {
-                double pLeft = Left.Probability(sample);
-                double pRight = Right.Probability(sample);
-                // If there would be an early out for multiplication with 0 (without having to evaluate the recursive calls), the final else statement would be suffice
-                if (pLeft <= 0 && pRight <= 0) {
-                    return 0;
-                } else if (pLeft <= 0) {
-                    return (1 - Left.CumulativeDistribution(sample)) * pRight;
-                } else if (pRight <= 0) {
-                    return (1 - Right.CumulativeDistribution(sample)) * pLeft;
-                } else {
-                    return (1 - Left.CumulativeDistribution(sample)) * pRight + (1 - Right.CumulativeDistribution(sample)) * pLeft;
-                }
-            }
-        }
-
-        new double CumulativeDistribution(T sample) {
-            double l = Left.CumulativeDistribution(sample);
-            double r = Right.CumulativeDistribution(sample);
-            return l + r - l * r;
-        }
     }
 }

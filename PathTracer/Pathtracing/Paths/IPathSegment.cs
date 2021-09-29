3061f43a27f7830960393b997419a41ab4651bf3
Theo Harkes <taharkes@gmail.com>
2021-09-29 18:40:06 +0200

    Added: Paths namespace

---
+++ PathTracer/Pathtracing/Paths/IPathSegment.cs
@@ -0,0 +1,17 @@
+﻿using PathTracer.Spectra;
+using System.Collections.Generic;
+
+namespace PathTracer.Pathtracing.Paths {
+    public interface IPathSegment {
+        /// <summary> The predecessor of this <see cref="IPathSegment"/> (if it's not a root node) </summary>
+        IPathSegment? Predecessor { get; }
+        /// <summary> Whether this <see cref="IPathSegment"/> has a <see cref="Predecessor"/> or is a root </summary>
+        bool Root => Predecessor is null;
+        /// <summary> The successors of this <see cref="IPathSegment"/> (if there are any) </summary>
+        IEnumerable<IPathSegment> Successors { get; }
+        
+        ISpectrum Radiance { get; }
+        ISpectrum Importance { get; }
+        ISpectrum Absorption { get; }
+    }
+}

fcf9ed35f85e8428fa9f1eb2ab36ac05d6d9902f
Theo Harkes <taharkes@gmail.com>
2021-10-18 17:28:05 +0200

    Commented: IPDF

---
+++ PathTracer/Pathtracing/Distributions/Distance/DistanceQuery.cs
@@ -0,0 +1,22 @@
+﻿using PathTracer.Pathtracing.Boundaries;
+using PathTracer.Pathtracing.SceneDescription;
+using System;
+
+namespace PathTracer.Pathtracing.Distributions.Distance {
+    public abstract class DistanceQuery : IDistanceQuery {
+        public abstract ISceneObject SceneObject { get; }
+        public abstract IBoundaryCollection Boundaries { get; }
+        public abstract IDistanceDistribution DistanceDistribution { get; }
+        public abstract bool SingleSolution { get; }
+        public abstract double DomainSize { get; }
+
+        public abstract bool Contains(ISurfacePoint sample);
+        public abstract double CumulativeDistribution(ISurfacePoint sample);
+        public abstract double Probability(ISurfacePoint sample);
+        public abstract ISurfacePoint Sample(Random random);
+    }
+
+    public class SingleDistanceQuery : DistanceQuery { }
+    public class ExponentialDistanceQuery : DistanceQuery { }
+    public class SumDistanceQuery : DistanceQuery { }
+}
--- PathTracer/Pathtracing/Distributions/IPDF.cs
+++ PathTracer/Pathtracing/Distributions/IPDF.cs
@@ -3,21 +3,37 @@
 namespace PathTracer.Pathtracing.Distributions {
     /// <summary> A generic probability distribution/density function </summary>
     public interface IPDF {
+        /// <summary> Whether the <see cref="IPDF"/> has a single solution </summary>
         bool SingleSolution { get; }
+        /// <summary> The size of the domain of the <see cref="IPDF"/> </summary>
         double DomainSize { get; }
     }
 
     /// <summary> A probability distribution/density function </summary>
     /// <typeparam name="T">The variable of the distribution function</typeparam>
     public interface IPDF<T> : IPDF {
+        /// <summary> Sample the <see cref="IPDF{T}"/> </summary>
+        /// <param name="random">The <see cref="Random"/> to use for sampling</param>
+        /// <returns>A <paramref name="random"/> <see cref="T"/></returns>
         T Sample(Random random);
+
+        /// <summary> Check whether the <see cref="IPDF{T}"/> contains a <paramref name="sample"/> in its domain </summary>
+        /// <param name="sample">The sample to check</param>
+        /// <returns>Whether the <paramref name="sample"/> is in the domain of the <see cref="IPDF{T}"/></returns>
         bool Contains(T sample);
+
+        /// <summary> Get the probability (density) of a <paramref name="sample"/> in the <see cref="IPDF{T}"/> </summary>
+        /// <param name="sample">The sample to get the probability for</param>
+        /// <returns>The probability (density) of the <paramref name="sample"/></returns>
         double Probability(T sample);
     }
 
     /// <summary> A cummulative distribution/density function </summary>
     /// <typeparam name="T">The variable of the distribution function</typeparam>
     public interface ICDF<T> : IPDF<T> {
+        /// <summary> Get the cummulative probability of a <paramref name="sample"/> in the <see cref="ICDF{T}"/> </summary>
+        /// <param name="sample">The sample to get the cummulative probability for</param>
+        /// <returns>The cummulative probability of the <paramref name="sample"/></returns>
         double CumulativeDistribution(T sample);
     }
 

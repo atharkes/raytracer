633cf584ff83861e6ad5c9670c32714f7561e9e5
Theo <taharkes@gmail.com>
2022-01-21 16:08:37 +0100

    Fixed: bug when multiple volumetrics overlap

--- PathTracer/Pathtracing/Distributions/DistanceQuery/CombinedDistanceQuery.cs
+++ PathTracer/Pathtracing/Distributions/DistanceQuery/CombinedDistanceQuery.cs
@@ -2,28 +2,49 @@
 using PathTracer.Pathtracing.Distributions.Distance;
 using PathTracer.Pathtracing.Distributions.Probabilities;
 using PathTracer.Pathtracing.SceneDescription.SceneObjects;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
 namespace PathTracer.Pathtracing.Distributions.DistanceQuery {
-    public class CombinedDistanceQuery : IDistanceQuery {
+    public class CombinedDistanceQuery : IDistanceQuery, ICollection<IDistanceQuery> {
         /// <summary> The <see cref="IDistanceDistribution"/> of the <see cref="CombinedDistanceQuery"/> </summary>
-        public IDistanceDistribution DistanceDistribution => new CombinedDistanceDistribution(queries.Select(q => q.DistanceDistribution).ToArray());
+        public IDistanceDistribution DistanceDistribution => new CombinedDistanceDistribution(Queries.Select(q => q.DistanceDistribution).ToArray());
 
-        readonly SortedSet<IDistanceQuery> queries;
+        public int Count => Queries.Count;
+        public bool IsReadOnly => Queries.IsReadOnly;
+
+        protected readonly ICollection<IDistanceQuery> Queries;
 
         public CombinedDistanceQuery(params IDistanceQuery[] queries) {
-            this.queries = new(queries, Comparer<IDistanceQuery>.Create((a, b) => a.DistanceDistribution.Domain.Entry.CompareTo(b.DistanceDistribution.Domain.Entry)));
+            List<IDistanceQuery> simpleQueries = new();
+            foreach (IDistanceQuery query in queries) {
+                if (query is CombinedDistanceQuery combinedQuery) {
+                    simpleQueries.AddRange(combinedQuery.Queries);
+                } else {
+                    simpleQueries.Add(query);
+                }
+            }
+            IComparer<IDistanceQuery> comparer = Comparer<IDistanceQuery>.Create((a, b) => a.DistanceDistribution.Domain.Entry.CompareTo(b.DistanceDistribution.Domain.Entry));
+            Queries = new SortedSet<IDistanceQuery>(simpleQueries, comparer);
         }
 
-        public bool Contains(IPrimitive primitive) => queries.Any(q => q.Contains(primitive));
+        public void Add(IDistanceQuery item) => Queries.Add(item);
+        public void Clear() => Queries.Clear();
+        public bool Contains(IDistanceQuery item) => Queries.Contains(item);
+        public void CopyTo(IDistanceQuery[] array, int arrayIndex) => Queries.CopyTo(array, arrayIndex);
+        public bool Remove(IDistanceQuery item) => Queries.Remove(item);
+        public IEnumerator<IDistanceQuery> GetEnumerator() => Queries.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => Queries.GetEnumerator();
+
+        public bool Contains(IPrimitive primitive) => Queries.Any(q => q.Contains(primitive));
 
         public WeightedPMF<IPrimitive> GetPrimitives(Position1 sample) {
             Debug.Assert(DistanceDistribution.Contains(sample));
             List<(WeightedPMF<IPrimitive>, double)> pmfs = new();
             double cdf = DistanceDistribution.CumulativeProbability(sample);
-            var contains = queries.Where(d => d.DistanceDistribution.Contains(sample));
+            var contains = Queries.Where(d => d.DistanceDistribution.Contains(sample));
             if (contains.Count() == 1) {
                 IDistanceQuery query = contains.First();
                 double queryCdf = query.DistanceDistribution.CumulativeProbability(sample);
@@ -41,7 +62,9 @@ namespace PathTracer.Pathtracing.Distributions.DistanceQuery {
                     }
                     double othersCdf = (queryCdf - cdf) / (queryCdf - 1);
                     double probabilityDensity = query.DistanceDistribution.Probability(sample) * (1 - othersCdf);
-                    pmfs.Add((pmf, probabilityDensity));
+                    if (probabilityDensity > 0) {
+                        pmfs.Add((pmf, probabilityDensity));
+                    }
                 }
                 return new WeightedPMF<IPrimitive>(pmfs.ToArray());
             }

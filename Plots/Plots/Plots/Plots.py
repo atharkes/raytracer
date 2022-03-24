05ca2a8c7f6d4c514d69051a2caa6f7c08f76f40
Theo Harkes <taharkes@gmail.com>
2022-03-24 17:31:08 +0100

    Added: cdf to uniform graphs

--- Plots/Plots/Plots/Plots.py
+++ Plots/Plots/Plots/Plots.py
@@ -2,32 +2,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-start = 0.0
-end = 1.0
-steps = 10000
 
-size = end - start
+
+distribution_start = 0.0
+distribution_end = 1.0
+steps = 1000
+
+graph_start = distribution_start - 0.1
+graph_end = distribution_end + 0.1
+
+size = distribution_end - distribution_start
 probability_density = 1 / size
-step = size / steps
+step_size = size / steps
 
-distances = np.arange(start, end, step)
-material_densities = 1 / (end - distances)
-probability_densities = np.repeat(probability_density, steps)
+distances = np.arange(graph_start, graph_end, step_size)
+material_densities = []
+probability_densities = []
+cummulative_probabilities = []
+for distance in distances:
+    if distribution_start <= distance <= distribution_end:
+        if (distribution_end - distance) == 0:
+            material_densities.append(float('inf'))
+        else:
+            material_densities.append(1 / (distribution_end - distance))
+        probability_densities.append(probability_density)
+    else:
+        material_densities.append(0)
+        probability_densities.append(0)
+    cummulative_probabilities.append(min(max(distance - distribution_start, 0), size) * probability_density)
 
 
-fig1, (probability_plot, density_plot) = plt.subplots(2, 1, sharex=True)
+fig1, (material_density_plot, probability_density_plot, cummulative_probability_plot) = plt.subplots(3, 1, sharex=True)
 
-density_plot.plot(distances, material_densities, linewidth=3)
-density_plot.fill_between(distances, material_densities, 0, facecolor='lightblue')
-density_plot.set_yscale('log')
-density_plot.set_ylim([material_densities[0] / 10, material_densities[-2]])
-density_plot.set_xlabel('distance')
-density_plot.set_ylabel('material density')
+material_density_plot.grid(axis='y', color='lightgray', linestyle='--')
+material_density_plot.fill_between(distances, material_densities, 0, facecolor='lightblue')
+material_density_plot.plot(distances, material_densities, linewidth=3)
+material_density_plot.set_yscale('log')
+material_density_plot.set_ylim([probability_density / 10, max(material_densities)])
+material_density_plot.set_ylabel('material density')
 
-probability_plot.plot(distances, probability_densities, linewidth=2, color='red')
-probability_plot.fill_between(distances, probability_densities, 0, facecolor='lightcoral', edgecolor='indianred', hatch='////')
-probability_plot.set_ylim([0, math.log(steps, 10)])
-probability_plot.set_ylabel('probability density')
+probability_density_plot.grid(axis='y', color='lightgray', linestyle='--')
+probability_density_plot.fill_between(distances, probability_densities, 0, facecolor='lightcoral', edgecolor='firebrick', hatch='///')
+probability_density_plot.plot(distances, probability_densities, linewidth=3, color='crimson')
+probability_density_plot.set_ylim([0, math.log(steps, 10) + 1])
+probability_density_plot.set_ylabel('probability density')
+
+cummulative_probability_plot.grid(axis='y', color='lightgray', linestyle='--')
+cummulative_probability_plot.fill_between(distances, cummulative_probabilities, 0, facecolor='plum', edgecolor='purple', hatch='///')
+cummulative_probability_plot.plot(distances, cummulative_probabilities, linewidth=3, color='darkviolet')
+cummulative_probability_plot.set_ylim([0, math.log(steps, 10) + 1])
+cummulative_probability_plot.set_ylabel('cumulative probability')
+cummulative_probability_plot.set_xlim([graph_start, graph_end])
+cummulative_probability_plot.set_xlabel('distance')
 
 
 fig2, (combinedPlot) = plt.subplots(1, 1, sharex=True)
@@ -38,7 +64,7 @@ combinedPlot.plot(distances, probability_densities, c='blue', label='Probability
 combinedPlot.fill_between(distances, probability_densities, 0, facecolor='none', edgecolor='blue', hatch='///')
 combinedPlot.legend(loc='upper left')
 combinedPlot.set_yscale('log')
-combinedPlot.set_ylim([0.0001, material_densities[-2]])
+combinedPlot.set_ylim([0.0001, max(material_densities)])
 combinedPlot.set_xlabel('distance')
 combinedPlot.set_ylabel('density')
 

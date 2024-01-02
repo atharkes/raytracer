61c9c8ad368810a88481ed9b8e317392da94105e
Theo <taharkes@gmail.com>
2024-01-02 22:28:36 +0100

    Added: Point & Point

--- Algebra/Geometry/Operators.cs
+++ Algebra/Geometry/Operators.cs
@@ -1,6 +1,12 @@
 ﻿namespace Algebra.Geometry;
 public class Operators {
-    public static Line Join(Point p1, Point p2) {
-        return new()
-    }
+    public static Line Join(Point p1, Point p2)
+        => new(
+            p1.E021 * p2.E013 - p1.E013 * p2.E021,
+            p1.E032 * p2.E021 - p1.E021 * p2.E032,
+            p1.E013 * p2.E032 - p1.E032 * p2.E013,
+            p1.E021 * p2.E123 - p1.E123 * p2.E021,
+            p1.E013 * p2.E123 - p1.E123 * p2.E013,
+            p1.E032 * p2.E123 - p1.E123 * p2.E032
+        );
 }
